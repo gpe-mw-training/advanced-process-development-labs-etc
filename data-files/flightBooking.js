5bc69fc5cd1227115d9e26125ea76eb8c00e459e
dtorresf <dtorresf@redhat.com>
2017-01-11 15:30:45 -0600

    https://github.com/redhat-gpe/advanced_process_development/issues/49

---
+++ data-files/flightBooking.js
@@ -0,0 +1,11 @@
+{
+	"flightBooking": {
+		"Booking": {
+			"carrier": "AA",
+			"flightNr": "AAD3345",
+      "travelClass": "BUSINESS",
+      "type": "DOMESTIC",
+      "booking": "JAN 21 2017"
+		}
+	}
+}
